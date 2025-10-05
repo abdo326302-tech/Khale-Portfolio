339b501ec7b73e8aa373a63bde2d0151791db04c
abdo326302-tech <abdo326302@gmail.com>
2025-10-05 17:19:44 +0300

    Add client/src/components/examples/SoftSkills.tsx

---
+++ client/src/components/examples/SoftSkills.tsx
@@ -0,0 +1,13 @@
+import SoftSkills from '../SoftSkills'
+
+export default function SoftSkillsExample() {
+  const skills = [
+    "Leadership",
+    "Problem Solving",
+    "Presentation Skills",
+    "Public Speaking",
+    "Communication Skills"
+  ];
+
+  return <SoftSkills skills={skills} />
+}
