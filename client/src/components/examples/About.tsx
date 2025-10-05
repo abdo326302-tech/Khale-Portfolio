3e91c0bf786eb899849fa267adfc3a43a8ec963e
abdo326302-tech <abdo326302@gmail.com>
2025-10-05 17:19:41 +0300

    Add client/src/components/examples/About.tsx

---
+++ client/src/components/examples/About.tsx
@@ -0,0 +1,9 @@
+import About from '../About'
+
+export default function AboutExample() {
+  return (
+    <About
+      content="With a solid foundation in C++, OOP, Databases, and Data Analysis, I bring a problem-solving mindset and deliver end-to-end solutions. My goal is to learn continuously, collaborate effectively, and build impactful software projects that deliver measurable results."
+    />
+  )
+}
