9e37b55949149ff193a31497e384b71079f7f348
abdo326302-tech <abdo326302@gmail.com>
2025-10-05 17:19:42 +0300

    Add client/src/components/examples/Footer.tsx

---
+++ client/src/components/examples/Footer.tsx
@@ -0,0 +1,9 @@
+import Footer from '../Footer'
+
+export default function FooterExample() {
+  return (
+    <Footer
+      onExploreClick={() => console.log('Explore more clicked')}
+    />
+  )
+}
